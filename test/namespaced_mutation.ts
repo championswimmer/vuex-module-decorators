fe447f22c39ace4cabec92c4bb02f057b1a124e4
Arnav Gupta <arnav@codingblocks.com>
2018-05-01 04:11:08 +0530

    more tests
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ test/namespaced_mutation.ts
@@ -0,0 +1,31 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+Vue.use(Vuex)
+import {Action, Module, Mutation, MutationAction, VuexModule} from '../dist'
+import {expect} from 'chai'
+
+@Module
+class MyModule extends VuexModule {
+  count = 0
+
+  @Mutation
+  incrCount(delta) {
+    this.count += delta
+  }
+
+}
+
+const store = new Vuex.Store({
+  modules: {
+    mm: MyModule
+  }
+})
+
+describe('committing mutation works', () => {
+  it('should update count', function () {
+
+    store.commit('incrCount', 5)
+    expect(parseInt(store.state.mm.count)).to.equal(5)
+
+  })
+})
