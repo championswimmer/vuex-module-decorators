dded34abc32db0cfadcd4cebbc5bc945221369c0
Pete Johanson <peter@peterjohanson.com>
2018-07-21 19:34:15 -0400

    Tweak test to demonstrate state isolation.

---
+++ test/state_factory_state_isolation.ts
@@ -0,0 +1,66 @@
+import Vuex, {Module as Mod} from 'vuex'
+import Vue from 'vue'
+Vue.use(Vuex)
+import {Action, Module, Mutation, MutationAction, VuexModule} from '../dist'
+import {expect} from 'chai'
+
+@Module({ stateFactory: true, namespaced: true })
+class FactoryModule extends VuexModule {
+  wheels = 2
+
+  @Mutation
+  incrWheels(extra) {
+    this.wheels += extra
+  }
+
+  get axles() {
+    return (this.wheels / 2)
+  }
+
+}
+
+@Module({ namespaced: true })
+class StateObjectModule extends VuexModule {
+  wheels = 2
+
+  @Mutation
+  incrWheels(extra) {
+    this.wheels += extra
+  }
+
+  get axles() {
+    return (this.wheels / 2)
+  }
+
+}
+
+const store = new Vuex.Store({
+  modules: {
+    factoryModA: FactoryModule,
+    factoryModB: FactoryModule,
+    objectModA: StateObjectModule,
+    objectModB: StateObjectModule,
+  }
+})
+
+describe('state isolation', () => {
+  it('should share state by default when reused', function () {
+
+    store.commit('objectModA/incrWheels', 4)
+    const axlesA = store.getters['objectModA/axles']
+    const axlesB = store.getters['objectModB/axles']
+
+    expect(axlesA).to.equal(3)
+    expect(axlesB).to.equal(3)
+
+  })
+  it('should isolate state using factories when reused', function () {
+
+    store.commit('factoryModA/incrWheels', 4)
+    const axlesA = store.getters['factoryModA/axles']
+    const axlesB = store.getters['factoryModB/axles']
+
+    expect(axlesA).to.equal(3)
+    expect(axlesB).to.equal(1)
+  })
+})
