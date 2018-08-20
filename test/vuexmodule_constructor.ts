cd478ee72e494865bd51ff4417b398fe31eef01d
Arnav Gupta <arnav@codingblocks.com>
2018-08-21 04:53:07 +0530

    add test for VuexModule constructor

---
+++ test/vuexmodule_constructor.ts
@@ -0,0 +1,38 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import {VuexModule} from '../dist'
+import {expect} from 'chai'
+
+Vue.use(Vuex)
+
+const vehicle = new VuexModule({
+  state: {
+    wheels: 2
+  },
+  mutations: {
+    incrWheels(state, extra) {
+      state.wheels += extra
+    }
+  },
+  getters: {
+    axles(state, rootState) {
+      return state.wheels / 2
+    }
+  }
+})
+
+const store = new Vuex.Store({
+  modules: {
+    vehicle
+  }
+})
+
+describe('fetching via getters works', () => {
+  it('should increase axles', function () {
+
+    store.commit('incrWheels', 4)
+    const axles = store.getters.axles
+    expect(axles).to.equal(3)
+
+  })
+})
