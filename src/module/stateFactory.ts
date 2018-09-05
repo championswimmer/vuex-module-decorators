0c3f5ddaca19656ae6ee7dfc94bf3e459202a1d8
Arnav Gupta <arnav@codingblocks.com>
2018-09-05 14:38:20 +0530

    fix reserved keys leaking into state
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/module/stateFactory.ts
+++ src/module/stateFactory.ts
@@ -4,6 +4,11 @@ export function stateFactory <S>(module: Function & Mod<S, any>) {
   const state = new (module.prototype.constructor)({})
   const s = <S>{}
   Object.keys(state).forEach((key: string) => {
+    
+    if (['actions', 'getters', 'mutations', 'modules', 'state', 'namespaced'].indexOf(key) !== -1) {
+      // These are reserved keys, cannot use these
+      return;
+    }
     if (state.hasOwnProperty(key)) {
       if (typeof state[key] !== 'function') {
         (s as any)[key] = state[key]
