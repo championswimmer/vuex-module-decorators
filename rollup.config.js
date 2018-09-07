89fdc99b315186ef994b7b4bc83d90a4bdd8bf88
Arnav Gupta <arnav@codingblocks.com>
2018-09-08 03:45:26 +0530

    bundle as cjs, esm and min
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,42 @@
 import typescript from 'rollup-plugin-typescript2'
-import { terser } from 'rollup-plugin-terser'
+import {
+  terser
+} from 'rollup-plugin-terser'
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: {
-            file: "dist/index.min.js",
-            format: "cjs",
-            name: "vuex-module-decorators",
-            sourcemap: true,
-            exports: 'named',
-            interop: false,
-        },
-        external: [ "vuex" ],
-        plugins: [ typescript(), terser() ]
-    }
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        name: "vuex-module-decorators",
+        sourcemap: true,
+        exports: 'named',
+        interop: false,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        name: "vuex-module-decorators",
+        sourcemap: true,
+        exports: 'named',
+        interop: false,
+
+      }
+    ],
+    external: ["vuex"],
+    plugins: [typescript()]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: "dist/index.min.js",
+      format: "cjs",
+      name: "vuex-module-decorators",
+      sourcemap: true,
+    },
+    external: ["vuex"],
+    plugins: [typescript(), terser()]
+  }
 ];
