f310e032a8d4ec4a52e3a9b7611bbcfb98317c7c
Jett Cruise <40565538+talkid134@users.noreply.github.com>
2018-12-03 18:59:37 +0300

    Update mutable.ts

--- src/mutable.ts
+++ src/mutable.ts
@@ -1,18 +1,5 @@
 import { Module as Mod, Mutation as Mut, Payload } from 'vuex'
-import { VuexModule } from './vuexmodule'
 
-export function Mutable<T, R>(
-  target: T,
-  key: string | symbol,
-  descriptor: TypedPropertyDescriptor<(...args: any[]) => R>
-) {
-  const module = target.constructor as Mod<T, any>
-  if (!module.mutations) {
-    module.mutations = {}
-  }
-  const mutation: Mut<typeof target> = function(state: typeof target, payload: Payload) {
-    state['_' + (key as string)] = payload
-  }
-
-  module.mutations['auto:' + target.constructor.name + ':' + (key as string)] = mutation
+export function Mutable(target: any, key: string | symbol) {
+  target.state['auto:' + (key as string)] = target.state[key] // TODO: Target task, not work
 }
